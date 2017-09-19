81105f1990d711ba1fb8a70f293a7aecf9b9aa75
raphaelrocha22 <raphael.2205@gmail.com>
2017-09-18 22:49:37 -0300

    configurar servidor de email

---
+++ Projeto.Web/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using Projeto.Web.Models;
+using System.Net;
+
+namespace Projeto.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SendEmail(HomeViewModel model)
+        {
+            try
+            {
+                MailAddress from = new MailAddress("raphaelportfolio22@gmail.com", "Raphael portfolio");
+                MailAddress to = new MailAddress("raphael.2205@gmail.com", "Raphael Gmail");
+                MailMessage message = new MailMessage(from, to);
+                
+                message.Subject = "Contato portfolio";
+                message.Body = $"Nome: {model.name} \n" +
+                               $"Email: {model.email} \n" +
+                               $"Telefone: {model.phone} \n" +
+                               $"Mensagem: {model.message}";
+                
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                smtp.EnableSsl = true; // GMail requer SSL
+                smtp.Port = 587;       // porta para SSL
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network; // modo de envio
+                smtp.UseDefaultCredentials = false; // vamos utilizar credencias especificas
+
+                // seu usuário e senha para autenticação
+                smtp.Credentials = new NetworkCredential("raphaelportfolio22@gmail.com", "raphael22");
+
+                smtp.Send(message);
+
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+
+            return Json("sucesso");
+        }
+
+        public ActionResult DashboardVendas()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Projeto.Web/vendor/jquery/contact_me.js
@@ -0,0 +1,74 @@
+$(function() {
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var model = {
+          name: $("input#name").val(),
+          email: $("input#email").val(),
+          phone: $("input#phone").val(),
+          message: $("textarea#message").val()
+      };
+      var firstName = $("input#name").val(); // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $this.html("Enviando...");
+      $.ajax({
+          url: "/Home/SendEmail",
+        type: "POST",
+        data: model,
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Sua mensagem foi enviada. Obrigado pelo contato</strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Desculpe " + firstName + ", parece que o servidor de E-mail não está respondendo. Por favor, envie um E-mail diretamente para raphael.2205@gmail.com!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+              $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+              $('#sendMessageButton').html('Enviar');
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
